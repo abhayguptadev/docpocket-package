56c7142bd735c32ab09c3571799349772a8ebd28
Abhay Gupta <abhayg77657@gamil.com>
2026-03-06 14:34:23 +0530

    fix export allscreens and fix navigation

--- docpocket/lib/docpocket.dart
+++ docpocket/lib/docpocket.dart
@@ -1,11 +1,9 @@
 library docpocket;
 
-/// This is the main entry point for the DocPocket package.
-/// It exports only the public API that other developers should use.
-
 export 'src/docpocket_feature.dart';
 export 'src/models/category_model.dart';
 export 'src/models/document_model.dart';
 export 'src/services/app_provider.dart';
 export 'src/screens/home_screen.dart';
 export 'src/screens/category_screen.dart';
+export 'src/screens/splash_screen.dart';
