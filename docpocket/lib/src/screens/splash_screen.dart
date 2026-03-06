528626bc91b96783173efea63e2e6e349601fc01
Abhay Gupta <abhayg77657@gamil.com>
2026-03-06 15:05:48 +0530

    Removed internal Navigator from SplashScreen to maintain Provider scope

--- docpocket/lib/src/screens/splash_screen.dart
+++ docpocket/lib/src/screens/splash_screen.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _navigateToHome();
-  }
-
-  _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 2));
-    if (mounted) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
